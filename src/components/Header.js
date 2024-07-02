eb4173deca8d4916da06efe2d17745718ba7f9fb
unknown <64100803+d-ramirez88@users.noreply.github.com>
2024-07-02 10:12:28 -0600

    added style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import LittleLemonLogo from "./Logo.svg"
+import Nav from "./Nav";
 
 function Header() {
     return(
     <header>
       <img src={LittleLemonLogo} alt="little lemon logo"></img>
+      <Nav></Nav>
     </header>
     )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,18 @@
+import heroImage from "./restauranfood.jpg"
+
 function Hero() {
     return(
-        <section>
-            <img src = "heroImage.jpg" alt="food example"></img>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <h3>
-                We are a family owned Mediterranean restaurant, focused on
-                tradicional recipes served with a modern twist.
-            </h3>
-            <a href="\">Reserve a table</a>
+        <section id="heroSection">
+            <img src = {heroImage} alt="food example"></img>
+            <div>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <h3>
+                    We are a family owned Mediterranean restaurant, focused on
+                    tradicional recipes served with a modern twist.
+                </h3>
+                <a href="\">Reserve a table</a>
+            </div>
         </section>
     )
 }
