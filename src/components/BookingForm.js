95c281868ffce51250bb4e0075ff1f6486edb634
unknown <64100803+d-ramirez88@users.noreply.github.com>
2024-07-24 19:50:03 -0600

    validation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,14 +1,26 @@
 import {useState} from 'react'
 
 function BookingForm({availableTimes, handleDateChange, handleSubmit}) {
-    const [guests, setGuests] = useState(1)
+    const [guests, setGuests] = useState(0)
     const [occasion, setOccasion] = useState("Occasion")
 
     const [selectedTime, setSelectedTime] = useState()
     const [date,setDate] = useState(new Date().toLocaleDateString("en-CA"))
+
     function handleDate(e) {
         setDate(e.target.value)
         handleDateChange(new Date(e.target.value))
+    }
+
+    const dataValid = () => {
+        if (guests <= 0) {
+            return true
+        } else if (guests >=11) {
+            return true
+        } else {
+            return false
+        }
+        
     }
 
     return (
@@ -20,13 +32,13 @@ function BookingForm({availableTimes, handleDateChange, handleSubmit}) {
                 {availableTimes.map((time, index) => {return (<option key={index}>{time}</option>)})}
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" onChange={e=>{setGuests(e.target.value)}} value={guests}/>
+            <input type="number" min="1" max="10" id="guests" onChange={e=>{setGuests(e.target.value)}} value={guests}/>
             <label htmlFor="occasion">Occasion</label>
             <select id="occasion" onChange={e => setOccasion(e.target.value)} value={occasion}>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
-            <input type="submit" value="Make Your reservation" onClick={handleSubmit}/>
+            <input type="submit" value="Make Your reservation" onClick={handleSubmit} disabled={dataValid()}/>
         </form>
     )
 }
